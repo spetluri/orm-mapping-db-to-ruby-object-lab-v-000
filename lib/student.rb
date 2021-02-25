2ffed967059cbca5c9530d81a070b07015f417df
Sai Petluri <spetluri.1@gmail.com>
2021-02-25 03:42:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
       limit 1;
       SQL
       
-    DB[:conn].execute(sql,name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
 
